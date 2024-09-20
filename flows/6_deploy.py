068023cc1a5cf43d01b8cd476c8c5fe372970bb5
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2024-09-20 15:59:00 -0500

    remove extra failure

--- flows/6_deploy.py
+++ flows/6_deploy.py
@@ -19,14 +19,9 @@ def transform(data):
     _logger().info("Transforming data")
     return [str(d) for d in data]
 
-@task(retries=1)
+@task
 def load(transformed_data):
     _logger().info("Loading data")
-    if task_run.get_run_count() == 1:
-        for d in transformed_data:
-            # Simulate a failure
-            if d == "2":
-                raise Exception("Failed to load data")
 
 if __name__ == "__main__":
     flow.from_source(
